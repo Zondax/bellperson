0eb5ccc93742a88c78ccab6f1d160072d7b82b33
neithanmo <neithanmo@gmail.com>
2021-04-01 21:41:06 -0600

    Basic integration with the scheduler lib
    add logg:warn
    
    Add little comments
    
    update to use a unique id per device
    
    fix: allow support for both blst and pairing backends
    
    update to the latest scheduler and gpu tools
    
    add a fallback to cpu in the integration code if req is null
    
    rename member according to scheduler changes recently
    
    add a public function that is called from rust-fil-proof. and other cleanups

--- src/groth16/mod.rs
+++ src/groth16/mod.rs
@@ -13,6 +13,7 @@ mod mapped_params;
 mod params;
 mod proof;
 mod prover;
+mod prover_ext;
 mod verifier;
 mod verifying_key;
 
@@ -24,5 +25,6 @@ pub use self::mapped_params::*;
 pub use self::params::*;
 pub use self::proof::*;
 pub use self::prover::*;
+pub use self::prover_ext::*;
 pub use self::verifier::*;
 pub use self::verifying_key::*;
